eed585800ba4d3259e86a844022508a2c205310f
marcelooliveira <mclricardo@gmail.com>
2019-09-04 14:07:26 -0300

    1387-AspNetCoreParte5Modularizacao-video4.1-Criando um Novo Contexto.trec

---
+++ depois/Item04/CasaDoCodigo/Areas/Catalogo/Data/CatalogoDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Areas.Catalogo.Data
+{
+    public class CatalogoDbContext : DbContext
+    {
+        public CatalogoDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Categoria>(b =>
+            {
+                b.HasKey(t => t.Id);
+            });
+
+            modelBuilder.Entity<Produto>(b =>
+            {
+                b.HasKey(t => t.Id);
+            });
+        }
+    }
+}
