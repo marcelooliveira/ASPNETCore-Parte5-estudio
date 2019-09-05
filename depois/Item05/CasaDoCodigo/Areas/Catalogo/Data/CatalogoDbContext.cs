040b9ad2c7062a339b933fa3dc25ae2e43465aa8
marcelooliveira <mclricardo@gmail.com>
2019-09-05 12:18:38 -0300

    Item05 - início

--- depois/Item05/CasaDoCodigo/Areas/Catalogo/Data/CatalogoDbContext.cs
+++ depois/Item05/CasaDoCodigo/Areas/Catalogo/Data/CatalogoDbContext.cs
@@ -2,11 +2,9 @@
 using CasaDoCodigo.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Areas.Catalogo.Data
 {
